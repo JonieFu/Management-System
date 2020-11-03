6b055fbef97bb659a9f3032a6ce69e9c4a786471
JonieFu <joniefu@foxmail.com>
2020-11-03 10:12:11 +0800

    feat: 完成公司管理页面的表格编辑、删除、分页

--- src/views/company/index.vue
+++ src/views/company/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <el-form class="topForm" :model="form" :inline="true" size="small">
+    <el-form class="constant.topForm" :model="form" :inline="true" size="small">
       <el-row>
         <el-col :span="8" :lg="7">
           <el-form-item label="公司名称">
@@ -28,7 +28,7 @@
           <el-button
             type="primary"
             size="small"
-            @click="dialogFormVisible = true"
+            @click="constant.dialogFormVisible = true"
             icon="el-icon-circle-plus"
             >添加</el-button
           >
@@ -37,13 +37,13 @@
     </el-form>
     <el-dialog
       title="添加公司信息"
-      :visible.sync="dialogFormVisible"
+      :visible.sync="constant.dialogFormVisible"
       center
       width="500px"
     >
       <el-form :model="form" label-width="80px" size="small">
         <el-row>
-          <el-col >
+          <el-col>
             <el-form-item label="公司名称">
               <el-input
                 v-model="form.name"
@@ -52,25 +52,36 @@
               ></el-input>
             </el-form-item>
           </el-col>
-          </el-row>
-        <el-row> 
-            <el-col >
-              <el-form-item label="公司编号">
-                <el-input
-                  v-model="form.num"
-                  autocomplete="off"
-                  placeholder="请输入公司编号"
-                ></el-input>
-              </el-form-item>
-            </el-col>
-          </el-row>
+        </el-row>
         <el-row>
-          <el-col >
-            <el-form-item label="公司住址">
+          <el-col>
+            <el-form-item label="公司编号">
+              <el-input
+                v-model="form.num"
+                autocomplete="off"
+                placeholder="请输入公司编号"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col>
+            <el-form-item label="联系电话">
               <el-input
                 v-model="form.address"
                 autocomplete="off"
-                placeholder="请输入公司住址"
+                placeholder="请输入联系电话"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col>
+            <el-form-item label="公司地址">
+              <el-input
+                v-model="form.address"
+                autocomplete="off"
+                placeholder="请输入公司地址"
               ></el-input>
             </el-form-item>
           </el-col>
@@ -78,7 +89,7 @@
       </el-form>
       <el-row>
         <el-col :span="2" :offset="16">
-          <el-button @click="dialogFormVisible = false" size="small"
+          <el-button @click="constant.dialogFormVisible = false" size="small"
             >取 消</el-button
           >
         </el-col>
@@ -86,7 +97,7 @@
           <el-button
             type="primary"
             size="small"
-            @click=" messageNotification('添加成功') "
+            @click="messageNotification('添加成功')"
             >确 定</el-button
           >
         </el-col>
@@ -95,7 +106,7 @@
 
     <div class="table">
       <el-table
-        :data="tableData"
+        :data="constant.tableData"
         @selection-change="handleSelectionChange"
         :header-cell-style="{ background: '#eef1f6', color: '#606266' }"
       >
@@ -107,38 +118,19 @@
         ></el-table-column>
         <el-table-column
           align="center"
-          prop="num"
-          label="账号"
-        ></el-table-column>
-        <el-table-column
-          align="center"
           prop="name"
-          label="姓名"
+          label="公司名称"
         ></el-table-column>
         <el-table-column
           align="center"
-          prop="title"
-          label="岗位"
+          prop="num"
+          label="公司编号"
         ></el-table-column>
-        <el-table-column
-          align="center"
-          prop="department"
-          label="部门"
-        ></el-table-column>
-        <el-table-column align="center" prop="status" label="状态">
-          <template slot-scope="{ row }">
-            <div :class="row.status ? 'statusOn' : 'statusOff'">
-              {{ formatStatus(row.status) }}
-            </div>
-          </template>
+        <el-table-column align="center" prop="time" label="录入时间">
         </el-table-column>
-        <el-table-column align="center" prop="time" label="入职时间">
+        <el-table-column align="center" prop="tel" label="联系电话">
         </el-table-column>
-        <el-table-column align="center" prop="tel" label="电话">
-        </el-table-column>
-        <el-table-column align="center" prop="email" label="邮箱">
-        </el-table-column>
-        <el-table-column align="center" prop="address" label="住址">
+        <el-table-column align="center" prop="address" label="公司地址">
         </el-table-column>
         <el-table-column class="operating" label="操作" align="center">
           <template slot-scope="scope">
@@ -150,120 +142,70 @@
             >
             <el-dialog
               title="编辑员工信息"
-              :visible.sync="informationEditVisible"
+              :visible.sync="constant.informationEditVisible"
               center
               width="500px"
             >
-              <el-form :model="editData" label-width="80px">
+              <el-form :model="constant.editData" label-width="80px">
                 <el-row>
-                  <el-col :span="12">
-                    <el-form-item label="员工姓名">
+                  <el-col>
+                    <el-form-item label="公司名称">
                       <el-input
-                        v-model="editData.name"
+                        v-model="constant.editData.name"
                         autocomplete="off"
-                        placeholder="请输入员工姓名"
-                      ></el-input>
-                    </el-form-item>
-                  </el-col>
-                  <el-col :span="12"
-                    ><el-form-item label="员工岗位">
-                      <el-select
-                        v-model="editData.title"
-                        placeholder="请选择岗位"
-                      >
-                        <el-option label="前端工程师" value="1"></el-option>
-                        <el-option label="软件工程师" value="2"></el-option>
-                        <el-option label="架构师" value="3"></el-option>
-                      </el-select> </el-form-item
-                  ></el-col>
-                </el-row>
-                <el-row>
-                  <el-col :span="12">
-                    <el-form-item label="员工账号">
-                      <el-input
-                        v-model="editData.num"
-                        autocomplete="off"
-                        placeholder="请输入员工账号"
-                      ></el-input> </el-form-item
-                  ></el-col>
-                  <el-col :span="12">
-                    <el-form-item label="所属部门">
-                      <el-select
-                        v-model="editData.department"
-                        placeholder="请选择所属部门 "
-                      >
-                        <el-option label="项目管理部" value="1"></el-option>
-                        <el-option label="人力资源部" value="2"></el-option>
-                      </el-select>
-                    </el-form-item>
-                  </el-col>
-                </el-row>
-                <el-row
-                  ><el-col :span="12">
-                    <el-form-item label="员工状态">
-                      <el-select
-                        v-model="editData.status"
-                        placeholder=""
-                        style="width: 100%"
-                      >
-                        <el-option
-                          :label="statusText[0]"
-                          :value="statusList[0]"
-                        ></el-option>
-                        <el-option
-                          :label="statusText[1]"
-                          :value="statusList[1]"
-                        ></el-option>
-                      </el-select>
-                    </el-form-item>
-                  </el-col>
-                  <el-col :span="12">
-                    <el-form-item label="手机号码">
-                      <el-input
-                        v-model="editData.tel"
-                        autocomplete="off"
-                        placeholder="请输入手机号"
+                        placeholder="请输入公司名称"
                       ></el-input>
                     </el-form-item>
                   </el-col>
                 </el-row>
                 <el-row>
-                  <el-col :span="12">
-                    <el-form-item label="工作邮箱">
+                  <el-col>
+                    <el-form-item label="公司编号">
                       <el-input
-                        v-model="editData.email"
+                        v-model="constant.editData.num"
                         autocomplete="off"
-                        placeholder="请输入工作邮箱"
+                        placeholder="请输入公司编号"
                       ></el-input>
                     </el-form-item>
                   </el-col>
                 </el-row>
                 <el-row>
-                  <el-col :span="16">
-                    <el-form-item label="家庭住址">
+                  <el-col>
+                    <el-form-item label="联系电话">
                       <el-input
-                        v-model="editData.address"
+                        v-model="constant.editData.tel"
                         autocomplete="off"
-                        placeholder="请输入家庭住址"
+                        placeholder="请输入联系电话"
                       ></el-input>
                     </el-form-item>
+                  </el-col>
+                </el-row>
+                <el-row>
+                  <el-col>
+                    <el-form-item label="公司地址">
+                      <el-input
+                        v-model="constant.editData.address"
+                        autocomplete="off"
+                        placeholder="请输入公司地址"
+                      ></el-input>
+                    </el-form-item>
+                  </el-col>
+                </el-row>
+                <el-row>
+                  <el-col :span="2" :offset="16">
+                    <el-button
+                      @click="constant.informationEditVisible = false"
+                      size="small"
+                      >取消</el-button
+                    >
+                  </el-col>
+                  <el-col :span="2" :offset="3">
+                    <el-button type="primary" size="small" @click="confirmEdit"
+                      >确定</el-button
+                    >
                   </el-col>
                 </el-row>
               </el-form>
-              <el-row>
-                <el-col :span="2" :offset="16">
-                  <el-button
-                    @click="informationEditVisible = false"
-                    size="small"
-                    >取消</el-button
-                  >
-                </el-col>
-                <el-col :span="2" :offset="3">
-                  <el-button type="primary" size="small" @click="confirmEdit"
-                    >确定</el-button
-                  >
-                </el-col>
-              </el-row>
             </el-dialog>
             <el-button
               type="danger"
@@ -284,7 +226,7 @@
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :page-size="5"
-        :current-page="currentPage"
+        :current-page="constant.currentPage"
         :total="50"
       >
       </el-pagination>
@@ -293,87 +235,60 @@
 </template>
 
 <script>
-import tableJsonData from "./employee.json";
+import tableJsonData from "./company.json";
 
 export default {
   data() {
     return {
-      topForm: {},
-      formLabelWidth: "70px",
-      tableDataAmount: [],
-      editData: [],
-      statusText: ["离职", "在职"],
-      statusList: [0, 1],
+      constant: {
+        topForm: {},
+        tableDataAmount: [],
+        editData: [],
+        tableData: [],
+        dialogFormVisible: false,
+        informationEditVisible: false,
+        currentPage: 1,
+      },
       form: {
         name: "",
         num: "",
-        title: "",
-        department: "",
-        tel: "",
         address: "",
-        email: "",
-        status: null,
+        tel: "",
       },
-      tableData: [],
-      dialogFormVisible: false,
-      informationEditVisible: false,
-      currentPage: 1,
     };
   },
   mounted() {
-    this.tableData = tableJsonData.slice(0, 5);
+    this.constant.tableData = tableJsonData.slice(0, 5);
   },
   methods: {
-    messageNotification(val){
+    messageNotification(val) {
       this.$message({
-        message:val,
-        type:"success"
-      })
+        message: val,
+        type: "success",
+      });
     },
     confirmEdit() {
-      if (this.editData.status === this.statusText[0]) {
-        this.editData.status = this.statusList[0];
-      } else if (this.editData.status === this.statusText[1]) {
-        this.editData.status = this.statusList[1];
-      }
-      this.tableData[this.editData.index] = this.editData;
-      this.informationEditVisible = false;
-    },
-    formatStatus(value) {
-      return this.statusText[value];
+      this.constant.tableData[this.constant.editData.index] = this.constant.editData;
+      this.constant.informationEditVisible = false;
+      this.messageNotification("编辑成功");
     },
     onSubmit() {
       window.alert("搜索");
     },
     handleEdit(index, row) {
-      this.informationEditVisible = true;
       console.log(row);
-      this.editData = { ...row };
-      // this.editData = row
-      console.log(this.editData);
-      this.editData["index"] = index;
-      this.editData.status ? this.statusText[1] : this.statusText[0];
+      this.constant.informationEditVisible = true;
+      this.constant.editData = { ...row };
+      this.constant.editData["index"] = index;
     },
     removeRow(index, row) {
-      this.tableData.splice(index, 1);
+      this.constant.tableData.splice(index, 1);
+      this.messageNotification("删除成功");
     },
     handleSelectionChange(data) {
-      this.tableDataAmount = data;
+      this.constant.tableDataAmount = data;
     },
-    toggleType(status) {
-      if (status === 1) {
-        return "primary";
-      } else {
-        return "info";
-      }
-    },
-    toggleStyle(status) {
-      if (status === 1) {
-        return "cursor:default";
-      } else {
-        return true;
-      }
-    },
+
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`);
     },
@@ -381,43 +296,19 @@ export default {
       console.log(`当前页: ${val}`);
     },
     totalNum() {
-      return this.tableData.length;
+      return this.constant.tableData.length;
     },
     removeData() {
-      this.tableData = this.tableData.filter((item) => {
-        return this.tableDataAmount.indexOf(item) === -1;
+      this.constant.tableData = this.constant.tableData.filter((item) => {
+        return this.constant.tableDataAmount.indexOf(item) === -1;
       });
     },
   },
 };
 </script>
 <style lang="scss" scoped>
-.topForm {
-  .el-input {
-    width: 180px;
-  }
-  .el-select {
-    width: 180px;
-  }
-}
 .table {
   margin-top: 20px;
-  .statusOn {
-    width: 4em;
-    margin-left: auto;
-    margin-right: auto;
-    color: #ffffff;
-    border-radius: 4px;
-    background: #409eff;
-  }
-  .statusOff {
-    width: 4em;
-    margin-left: auto;
-    margin-right: auto;
-    color: #ffffff;
-    border-radius: 4px;
-    background: #909399;
-  }
 }
 .footer {
   display: flex;
